d2a483a14e47954fab08ca710c0cbfa4df59e1d7
bhavik1215 <parmarbhavik1205@gmail.com>
2025-11-16 12:40:23 +0530

    Add files via upload

---
+++ fifo_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/01/2025 11:39:47 PM
+// Design Name: 
+// Module Name: fifo_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fifo_tb(
+
+    );
+endmodule
